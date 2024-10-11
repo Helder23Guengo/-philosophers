3bb19ecc15acdc9f05a2c3ff8f5e0c0505d183f8
Helder Esteves Guengo <hguengo@c1r8s6.42luanda.com>
2024-10-11 16:44:44 +0100

    philo

--- philo_life.c
+++ philo_life.c
@@ -6,7 +6,7 @@
 /*   By: hguengo <hguengo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 09:51:30 by hguengo           #+#    #+#             */
-/*   Updated: 2024/10/10 14:12:08 by hguengo          ###   ########.fr       */
+/*   Updated: 2024/10/11 14:33:42 by hguengo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ int		get_current_last(size_t time)
 
 int		live(t_philosopher *philo, t_arg *argay)
 {
+    int     is_dead;
+    
     pthread_mutex_lock(&argay->dead_mutex);
 	if (argay->is_dead)
 	{
 		pthread_mutex_unlock(&argay->dead_mutex);
-		return 0;
+		return (0);
 	}
-    int is_dead = argay->is_dead;
+    is_dead = argay->is_dead;
     pthread_mutex_unlock(&argay->dead_mutex);
-	
-	if(is_dead)
+
+	if (is_dead)
 		return (0);
     return (1); 
 }
@@ -39,16 +41,16 @@ int		time_to_die(t_philosopher *philo, t_arg *argay)
     if (ft_hard_time > (philo->time_to_die + 1))
     {
         pthread_mutex_lock(&argay->dead_mutex);
-		live(philo, argay);
+		argay->is_dead = 1;
         pthread_mutex_unlock(&argay->dead_mutex);
 
         pthread_mutex_lock(&argay->print_mutex);
         printf("\033[1;31m%ld %d morreu\033[0m\n", get_current_last(philo->arg->start_time) - (get_current_last(philo->arg->start_time) - philo->time_to_die), philo->id);
         pthread_mutex_unlock(&argay->print_mutex);
 
-        return 1;
+        return (1);
     }
-    return 0;
+    return (0);
 }
 
 int		time_to_eat(t_philosopher *philo)
@@ -63,23 +65,25 @@ int		time_to_eat(t_philosopher *philo)
 	philo->last_to_eat = get_current_time();
 	philo->meals++;
 	usleep(philo->time_to_eat * 1000);
-
-	pthread_mutex_unlock(philo->right_fork);
+    
 	pthread_mutex_unlock(philo->left_fork);
-
-	return 0;
+	pthread_mutex_unlock(philo->right_fork);
+	return (0);
 }
 
 void	*philo_monitor(void *args)
 {
     t_arg *arg = (t_arg *)args;
-    int cont = 0;
-    while (1) {
+    int cont;
+
+    cont = 0;
+    while (1)
+    {
         cont = 0;
         while (cont < arg->num_philosophers) {
             if (time_to_die(&(arg->philosophers[cont]), arg))
 			{
-                return NULL;
+                return (NULL);
             }
             cont++;
         }
@@ -97,18 +101,18 @@ void	*philo_life(void *arg)
         if (philo->arg->max_meals == -1 || philo->meals < philo->arg->max_meals)
 		{
             if (time_to_die(philo, arg_dead))
-                	break;
+                	break ;
             print_status(philo, "está pensando");
             if (time_to_die(philo, arg_dead))
-                	break;
+                	break ;
             time_to_eat(philo);
             print_status(philo, "está dormindo");
             if (time_to_die(philo, arg_dead))
-                	break;
+                	break ;
             usleep(philo->time_to_sleep * 1000);
         }
 		else
             break;
     }
-    return NULL;
+    return (NULL);
 }
--- status.c
+++ status.c
@@ -6,7 +6,7 @@
 /*   By: hguengo <hguengo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 12:48:03 by hguengo           #+#    #+#             */
-/*   Updated: 2024/10/10 14:10:45 by hguengo          ###   ########.fr       */
+/*   Updated: 2024/10/11 14:31:22 by hguengo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 long    get_current_time()
 {
     struct timeval tv;
-    
+
     gettimeofday(&tv, NULL);
     return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
@@ -25,12 +25,15 @@ int     print_status(t_philosopher *philo, const char *status)
     t_arg *arg_dead = philo->arg;
 
     pthread_mutex_lock(&arg_dead->dead_mutex);
-    while(arg_dead->is_dead == 1)
-        break ;
+    if (arg_dead->is_dead)
+    {
+        pthread_mutex_unlock(&arg_dead->dead_mutex);
+        return (1);   
+    }
     pthread_mutex_unlock(&arg_dead->dead_mutex);
     pthread_mutex_lock(&arg_dead->print_mutex);
     printf("%ld %d %s\n", get_current_time() - arg_dead->start_time, philo->id, status);
     pthread_mutex_unlock(&arg_dead->print_mutex);
-    return 0;
+    return (0);
 }
 
