c4758cb3018077c11a6da8c0dbeb4b7526a9d6b9
Helder Esteves Guengo <hguengo@c1r7s1.42luanda.com>
2024-10-15 16:24:45 +0100

    philo

--- status.c
+++ status.c
@@ -6,34 +6,35 @@
 /*   By: hguengo <hguengo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 12:48:03 by hguengo           #+#    #+#             */
-/*   Updated: 2024/10/15 13:52:43 by hguengo          ###   ########.fr       */
+/*   Updated: 2024/10/15 16:24:32 by hguengo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-long    get_current_time()
+long	get_current_time(void)
 {
-    struct timeval tv;
+	struct timeval	tv;
 
-    gettimeofday(&tv, NULL);
-    return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
-int     print_status(t_philosopher *philo, const char *status)
+int	print_status(t_philosopher *philo, const char *status)
 {
-    t_arg *arg_dead = philo->arg;
+	t_arg	*arg_dead;
 
-    pthread_mutex_lock(&arg_dead->dead_mutex);
-    if (arg_dead->is_dead)
-    {
-        pthread_mutex_unlock(&arg_dead->dead_mutex);
-        return (1);   
-    }
-    pthread_mutex_unlock(&arg_dead->dead_mutex);
-    pthread_mutex_lock(&arg_dead->print_mutex);
-    printf("%ld %d %s\n", get_current_time() - arg_dead->start_time, philo->id, status);
-    pthread_mutex_unlock(&arg_dead->print_mutex);
-    return (0);
+	arg_dead = philo->arg;
+	pthread_mutex_lock(&arg_dead->dead_mutex);
+	if (arg_dead->is_dead)
+	{
+		pthread_mutex_unlock(&arg_dead->dead_mutex);
+		return (1);
+	}
+	pthread_mutex_unlock(&arg_dead->dead_mutex);
+	pthread_mutex_lock(&arg_dead->print_mutex);
+	printf("%ld %d %s\n", get_current_time() - arg_dead->start_time, philo->id,
+		status);
+	pthread_mutex_unlock(&arg_dead->print_mutex);
+	return (0);
 }
-
